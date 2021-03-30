b2b3ca536da9c5537ac995735f458dc4d202bd7b
Rex Elardo <rexelardo@gmail.com>
2021-03-30 18:26:49 -0400

    phew it finally works

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -10,9 +10,21 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsRegressor
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+myname = "RexElardo"
+EXPERIMENT_NAME = f"TaxifareModel_{myname}"
 
 class Trainer():
+    
+    
+    
+
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -22,8 +34,30 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
 
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+    
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
         distance_pipe = Pipeline([
@@ -55,7 +89,18 @@ class Trainer():
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         a=self.pipeline.predict(X_test)
+        self.mlflow_log_param('model', 'KnnRegressor')
+        self.mlflow_log_metric('rmse', compute_rmse(a,y_test))
         return compute_rmse(a,y_test)
+    
+    
+    # def train(self):
+
+    #     for model in ["KnnRegressor"]:
+    #         self.mlflow_create_run()
+    #         self.mlflow_log_metric("rmse", evaluate(X_test,y_test) )
+    #         self.mlflow_log_param("model", model)
+
 
 
 if __name__ == "__main__":
@@ -71,6 +116,7 @@ if __name__ == "__main__":
     # train
     train = Trainer(X_train,y_train)
     train.run()
+    # train.train()
 
     # evaluate
     print(train.evaluate(X_test, y_test))
